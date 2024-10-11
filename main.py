e43578e7e819a5e21f1798080faa35b9fbfa4a65
junseowon <junseowon@gmail.com>
2024-10-11 17:47:19 +0900

    texture

--- main.py
+++ main.py
@@ -2,25 +2,37 @@ import pygame
 import sys
 from settings import *
 from map import *
+from player import *
+from raycasting import *
+from object_renderer import *
 
 class Game:
     def __init__(self):
         pygame.init()
+        pygame.mouse.set_visible(False)
         self.screen = pygame.display.set_mode(RES)
         self.clock = pygame.time.Clock()
+        self.delta_time = 1
         self.new_game()
 
     def new_game(self):
         self.map = Map(self)
+        self.player = Player(self)
+        self.object_renderer =ObjectRenderer(self)        
+        self.raycasting = RayCasting(self)
 
     def update(self):
+        self.player.update()
+        self.raycasting.update()
         pygame.display.flip()
-        self.clock.tick(FPS)
+        self.delta_time = self.clock.tick(FPS)
         pygame.display.set_caption(f'{self.clock.get_fps() : .1f}')
 
     def draw(self):
         self.screen.fill('black')
-        self.map.draw()
+        self.object_renderer.draw()
+##        self.map.draw()
+##        self.player.draw()
 
     def check_events(self):
         for event in pygame.event.get():
---
+++ object_renderer.py
@@ -0,0 +1,37 @@
+import pygame
+from settings import *
+
+class ObjectRenderer:
+    def __init__(self, game):
+        self.game = game
+        self.screen = self.game.screen
+        self.wall_textures = self.load_wall_textures()
+        self.sky_image = self.get_texture('textures/sky.png', (WIDTH, HALF_HEIGHT))
+        self.sky_offset = 0
+
+    def draw(self):
+        self.draw_background()
+        self.render_game_objects()
+
+    def draw_background(self):
+        self.sky_offset = (self.sky_offset + 4.5 * self.game.player.rel) % WIDTH
+        self.screen.blit(self.sky_image, (-self.sky_offset, 0))
+        self.screen.blit(self.sky_image, (-self.sky_offset + WIDTH, 0))
+
+        pygame.draw.rect(self.screen, FLOOR_COLOR, (0, HALF_HEIGHT, WIDTH, HEIGHT))
+    
+    def render_game_objects(self):
+        list_objects = self.game.raycasting.objects_to_render
+        for depth, image, pos in list_objects:
+            self.screen.blit(image, pos)
+
+    @staticmethod
+    def get_texture(path, res=(TEXTURE_SIZE, TEXTURE_SIZE)):
+        texture = pygame.image.load(path).convert_alpha()
+        return pygame.transform.scale(texture, res)
+
+    def load_wall_textures(self):
+        return{
+            1: self.get_texture('textures/1.png'),
+            2: self.get_texture('textures/2.png')
+            }
