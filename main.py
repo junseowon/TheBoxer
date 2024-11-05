29432f32456062e8dca01733c6f561517e13afd9
junseowon <junseowon@gmail.com>
2024-11-05 21:57:30 +0900

    npc add
    [오후 9:57 2024-11-05]

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ class Game:
         self.screen = pygame.display.set_mode(RES)
         self.clock = pygame.time.Clock()
         self.delta_time = 1
+        self.global_trigger = False
+        self.global_event = pygame.USEREVENT + 0
+        pygame.time.set_timer(self.global_event, 40)
         self.new_game()
 
     def new_game(self):
@@ -37,17 +40,21 @@ class Game:
         self.delta_time = self.clock.tick(FPS)
         pygame.display.set_caption(f'{self.clock.get_fps() : .1f}')
 
-    def draw(self):        
-        self.object_renderer.draw()
-        self.weapon.draw()
-##        self.map.draw()
-##        self.player.draw()
+    def draw(self):
+        self.screen.fill('black')
+##        self.object_renderer.draw()
+##        self.weapon.draw()
+        self.map.draw()
+        self.player.draw()
 
     def check_events(self):
+        self.global_trigger = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 pygame.quit()
                 sys.exit()
+            elif event.type == self.global_event:
+                self.global_trigger = True
             self.player.single_fire_event(event)
 
     def run(self):
--- sound.py
+++ sound.py
@@ -6,3 +6,4 @@ class Sound:
         pygame.mixer.init()
         self.path = 'sound/'
         self.swing = pygame.mixer.Sound(self.path + 'Swing.mp3')
+        self.npc_pain = pygame.mixer.Sound(self.path + 'npc_pain.mp3')
